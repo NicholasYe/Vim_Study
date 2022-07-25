190b8e49725d833bf3abf6c020f0915eb28e5d1a
NicholasYe <2216393769@qq.com>
2022-07-25 08:30:13 +0800

    mac

--- Code/jsx.jsx
+++ Code/jsx.jsx
@@ -6,10 +6,8 @@ import Todo from "./Todo";
   TodoMVC
   1. add todo
   2. display todos
-  3. cross off todo
   4. show number of active todos
   5. filter all/active/complete
-  6. delete todo
   7. delete all complete
     7.1 only show if atleast one is complete
   8. button to toggle all on/off
@@ -17,7 +15,6 @@ import Todo from "./Todo";
 
 export default class TodoList extends React.Component {
   state = {
-    todos: [],
     todoToShow: "all",
     toggleAllComplete: true
   };
---
+++ Vim_command.md
@@ -0,0 +1,35 @@
+
+## Command of VIM
+
+### Save and Exit
+
+- :q
+  - quit the file
+- :q!
+  - quit the file without save
+- :w
+  - save the file
+- :wq
+  - quit and save the file
+
+
+### Move the Curser
+
+- j
+  - go down
+- k
+  - go up
+- l
+  - go right
+- h 
+  - go left
+  
+### insert mode
+
+- i
+  - enter insert mode
+- `esc`
+  - escape from insert mode
+
+### D command
+
